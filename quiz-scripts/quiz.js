c8e7ac25d39313b35fd3a0605f8703994a3f9c75
coderguy1777 <hill.jordan@charterschool.org>
2019-10-15 20:44:14 -0400

    more progress

--- quiz-scripts/quiz.js
+++ quiz-scripts/quiz.js
@@ -1,4 +1,6 @@
 let start_button = document.getElementById("quiz-start-btn");
+let curr_score = document.getElementById("quiz-score");
+let score_val = 0;
 
 start_button.addEventListener('click', () => {
     window.open('q_1.html', '_self', false);
