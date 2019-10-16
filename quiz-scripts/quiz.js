b52ecccbf664a00e9e7786be5dce7d1b8e66a2ea
Jordan Hill <33611224+coderguy1777@users.noreply.github.com>
2019-10-16 19:52:10 -0400

    Update quiz.js

--- quiz-scripts/quiz.js
+++ quiz-scripts/quiz.js
@@ -1,8 +1,5 @@
 let start_button = document.getElementById("quiz-start-btn");
-let curr_score = document.getElementById("quiz-score");
-let score_val = 0;
 
 start_button.addEventListener('click', () => {
     window.open('q_1.html', '_self', false);
-    localStorage.setItem("current_page", score_val);
 });
