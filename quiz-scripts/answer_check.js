b82c097f141dcfff2777a3ddf2337f8e1caf37f5
coderguy1777 <hill.jordan@charterschool.org>
2019-10-17 16:48:20 -0400

    Added final score screen, may add incorrect and correct screen, tbd, but also added proper score system as well, may change to make non negs possible

--- quiz-scripts/answer_check.js
+++ quiz-scripts/answer_check.js
@@ -1,17 +1,37 @@
-let score = 0;
+function open_q_2() {
+    window.open('q_2.html', '_self', false);
+}
+
+function open_q_3() {
+    window.open('q_3.html', '_self', false);
+}
+
+function open_q_4() {
+    window.open('q_4.html', '_self', false);
+}
+
+function open_q_5() {
+    window.open('q_5.html', '_self', false);
+}
+
+function open_q_6() {
+    window.open('q_6.html', '_self', false);
+}
+
+function open_results() {
+    window.open('final_score.html', '_self', false);
+}
+
 function answer_check(id) {
     switch(id) {
         case 0:
             alert('Correct!');
-            score+=1;
-            localStorage.setItem("score", score);
+            localStorage.setItem("score", 1);
             open_q_2();
             break;
         case 1:
             alert('Wrong!');
-            score-=1;
-            localStorage.setItem("score", score);
-            console.log(localStorage.getItem("score"));
+            localStorage.setItem("score", -1);
             open_q_2();
             break;
     }
@@ -35,12 +55,16 @@ function answer_check_2(id) {
 }
 
 function answer_check_3(id) {
+    let curr_score = localStorage.getItem("score");
+    let score_val = parseInt(curr_score);
     switch(id) {
         case 0:
+            localStorage.setItem("score", score_val + 1);
             alert('Correct!');
             open_q_4();
             break;
         case 1:
+            localStorage.setItem("score", score_val - 1);
             alert('Wrong!');
             open_q_4();
             break;
@@ -48,12 +72,16 @@ function answer_check_3(id) {
 }
 
 function answer_check_4(id) {
+    let curr_score = localStorage.getItem("score");
+    let score_val = parseInt(curr_score);
     switch(id) {
         case 0:
+            localStorage.setItem("score", score_val + 1);
             alert('Correct!');
             open_q_5();
             break;
         case 1:
+            localStorage.setItem("score", score_val - 1);
             alert('Wrong!');
             open_q_5();
             break;
@@ -61,12 +89,16 @@ function answer_check_4(id) {
 }
 
 function answer_check_5(id) {
+    let curr_score = localStorage.getItem("score");
+    let score_val = parseInt(curr_score);
     switch(id) {
         case 0:
+            localStorage.setItem("score", score_val + 1);
             alert('Correct!');
             open_q_6();
             break;
         case 1:
+            localStorage.setItem("score", score_val - 1);
             alert('Wrong!');
             open_q_6();
             break;
@@ -74,34 +106,18 @@ function answer_check_5(id) {
 }
 
 function answer_check_6(id) {
+    let curr_score = localStorage.getItem("score");
+    let score_val = parseInt(curr_score);
     switch(id) {
         case 0:
+            localStorage.setItem("score", score_val + 1);
             alert('Correct!');
-            window.open('index.html', '_self', false);
+            open_results();
             break;
         case 1:
+            localStorage.setItem("score", score_val - 1);
             alert('Wrong!');
-            window.open('index.html', '_self', false);
+            open_results();
             break;
     }
-}
-
-function open_q_2() {
-    window.open('q_2.html', '_self', false);
-}
-
-function open_q_3() {
-    window.open('q_3.html', '_self', false);
-}
-
-function open_q_4() {
-    window.open('q_4.html', '_self', false);
-}
-
-function open_q_5() {
-    window.open('q_5.html', '_self', false);
-}
-
-function open_q_6() {
-    window.open('q_6.html', '_self', false);
 }
---
+++ quiz-scripts/results.js
@@ -0,0 +1,38 @@
+let player_score = parseInt(localStorage.getItem('score'));
+let player_msg = document.getElementById("player_message");
+let score_box = document.getElementById("player_score");
+let button_no = document.getElementById('button-no');
+let button_yes = document.getElementById('button-yes');
+
+function get_score() {
+    score_box.innerHTML = player_score + '&nbsp' + "Points"
+    message_to_display();
+}
+
+function open_home() {
+    window.open('index.html', '_self', false);
+}
+
+function message_to_display() {
+    if(player_score <= 2) {
+        player_msg.innerHTML = "You did poorly, would you like to play again?"
+    } 
+
+    if(player_score < 0) {
+        player_msg.innerHTML = "Oh dear god you didn't do well, would you like to try again?";
+    }
+
+    if(player_score === 3 || player_score > 3) {
+        player_msg.innerHTML = "You did great, would you like to play again for fun?";
+    }
+}
+
+button_yes.addEventListener('click', () => {
+    localStorage.clear();
+    open_home();
+});
+
+button_no.addEventListener('click', () => {
+    alert('Thanks for playing!');
+    window.close();
+});
